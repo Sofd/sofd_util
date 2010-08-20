9d172c12e6171c8fd7ae8fc5f702262d421451e4
Olaf Klischat <olaf.klischat@sofd.de>
2010-08-20 19:11:58 +0000

    d.s.util: PriorityCache functionally completed incl. unit test. Still TODO: thread safety, base interface

--- src/de/sofd/util/PriorityCache.java
+++ src/de/sofd/util/PriorityCache.java
@@ -18,11 +18,11 @@ import de.sofd.lang.Function1;
  * "element cost function" {@link #getElementCostFunction()}, which maps each
  * element to its cost, a non-negative integer. This is commonly used for things
  * like the effective "memory consumption" of the element -- so then cache can
- * contain many small os fewer large elements or anything in between. In that
+ * contain many small or fewer large elements or anything in between. In that
  * case, {@link #getMaxTotalCost()} will be the maximum total memory consumption
  * up to which the cache will grow before it starts evicting low-priority
  * elements. In the default case, the element cost function will just return 1
- * for all element, so the {@link #getMaxTotalCost()} will effectively be the
+ * for all elements, so the {@link #getMaxTotalCost()} will effectively be the
  * maximum number of elements in the cache.
  * <p>
  * The time complexity of all basic operations will generally be O(1) (refer to
@@ -36,16 +36,22 @@ import de.sofd.lang.Function1;
  */
 public class PriorityCache<K, V> {
 
-    private final double lowPrio, highPrio;
-    private final int nBuckets;
-    private LinkedHashMap<K, V>[] buckets;
-    private final double bucketWidth;
+    protected class Entry {
+        V v;
+        double priority;
+        public Entry(V v, double priority) {
+            super();
+            this.v = v;
+            this.priority = priority;
+        }
+    }
+    
+    private final Map<K, Entry> entries = new HashMap<K, Entry>();
 
-    /**
-     * @invariant priorities.keySet() contains the same elements as \\union_{b
-     *            \in buckets} b.keySet()
-     */
-    private final Map<K, Double> priorities = new HashMap<K, Double>();
+    private final double lowPrio, highPrio;
+    private final int nBuckets, maxBucketNr;
+    private LinkedHashMap<K, Entry>[] buckets;
+    private final double bucketWidth;
 
     private final Function1<V, Integer> elementCostFunction;
 
@@ -94,28 +100,66 @@ public class PriorityCache<K, V> {
         this.lowPrio = lowPrio;
         this.highPrio = highPrio;
         this.nBuckets = nBuckets;
+        this.maxBucketNr = nBuckets - 1;
         this.buckets = new LinkedHashMap[nBuckets];
         for (int i = 0; i < nBuckets; i++) {
-            buckets[i] = new LinkedHashMap<K, V>(256, 0.75F, true);
+            buckets[i] = new LinkedHashMap<K, Entry>(256, 0.75F, true);
         }
         this.bucketWidth = (highPrio - lowPrio) / nBuckets;
         this.maxTotalCost = maxTotalCost;
         this.elementCostFunction = elementCostFunction;
     }
 
-    public void put(K k, V v, double priority) {
-        // TODO
-        throw new UnsupportedOperationException();
+    protected int prio2bucketNr(double prio) {
+        return Math.max(0, Math.min(maxBucketNr, (int)((prio - lowPrio) / bucketWidth)));
+    }
+    
+    public V put(K k, V v, double priority) {
+        V result = null;
+        Entry newE = new Entry(v, priority);
+        Entry oldE = entries.put(k, newE);
+        if (null != oldE) {
+            buckets[prio2bucketNr(oldE.priority)].remove(k);
+            totalCost -= elementCostFunction.run(oldE.v);
+            result = oldE.v;
+        }
+        buckets[prio2bucketNr(newE.priority)].put(k, newE);
+        totalCost += elementCostFunction.run(newE.v);
+        evictExcessElements();
+        return result;
     }
 
 
     public V get(K k) {
-        // TODO
-        throw new UnsupportedOperationException();
+        Entry e = entries.get(k);
+        if (e == null) {
+            return null;
+        } else {
+            return e.v;
+        }
+    }
+
+    public V remove(K k) {
+        Entry oldE = entries.remove(k);
+        if (oldE != null) {
+            buckets[prio2bucketNr(oldE.priority)].remove(k);
+            totalCost -= elementCostFunction.run(oldE.v);
+            return oldE.v;
+        } else {
+            return null;
+        }
     }
     
     public boolean contains(K k) {
-        return priorities.containsKey(k);
+        return entries.containsKey(k);
+    }
+
+    public int size() {
+        return entries.size();
+    }
+
+    public boolean isEmpty() {
+        return entries.isEmpty();
     }
 
     /**
@@ -123,8 +167,13 @@ public class PriorityCache<K, V> {
      * should be aware of that if needed.
      */
     public void setPriority(K k, double priority) {
-        // TODO
-        throw new UnsupportedOperationException();
+        Entry oldE = entries.remove(k);
+        if (oldE != null) {
+            buckets[prio2bucketNr(oldE.priority)].remove(k);
+            Entry newE = new Entry(oldE.v, priority);
+            entries.put(k, newE);
+            buckets[prio2bucketNr(newE.priority)].put(k, newE);
+        }
     }
 
     public int getCurrentTotalCost() {
@@ -136,6 +185,9 @@ public class PriorityCache<K, V> {
     }
 
     public void setMaxTotalCost(int maxTotalCost) {
+        if (maxTotalCost < 0) {
+            throw new IllegalArgumentException();
+        }
         this.maxTotalCost = maxTotalCost;
         evictExcessElements();
     }
@@ -144,9 +196,22 @@ public class PriorityCache<K, V> {
         return elementCostFunction;
     }
 
-    public void evictExcessElements() {
-        // TODO
-        throw new UnsupportedOperationException();
+    protected void evictExcessElements() {
+        while ((totalCost > maxTotalCost) && (entries.size() > 1)) {
+            // ^^^ ensure size >= 1 to always keep at least one element in, even if it alone exceeds maxTotalCost
+            for (int iBucket = 0; iBucket < nBuckets; iBucket++) {
+                LinkedHashMap<K, Entry> bucket = buckets[iBucket];
+                if (bucket.isEmpty()) {
+                    continue;
+                }
+                Map.Entry<K, Entry> oldestMapEntry = bucket.entrySet().iterator().next();
+                Entry oldestEntry = oldestMapEntry.getValue();
+                totalCost -= elementCostFunction.run(oldestEntry.v);
+                bucket.remove(oldestMapEntry.getKey());
+                entries.remove(oldestMapEntry.getKey());
+                break;
+            }
+        }
     }
 
 }
---
+++ test/de/sofd/util/PriorityCacheTest.java
@@ -0,0 +1,141 @@
+package de.sofd.util;
+
+import de.sofd.lang.Function1;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author olaf
+ */
+public class PriorityCacheTest {
+
+    protected static class EltValue {
+        private String id;
+        private int cost;
+
+        public EltValue(String id, int cost) {
+            this.id = id;
+            this.cost = cost;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public int getCost() {
+            return cost;
+        }
+    }
+
+    private static Function1<EltValue, Integer> costFunction = new Function1<EltValue, Integer>() {
+
+        @Override
+        public Integer run(EltValue p0) {
+            return p0.getCost();
+        }
+
+    };
+
+    public PriorityCacheTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSimpleMapping() {
+        System.out.println("SimpleMapping");
+        PriorityCache<String, EltValue> pc = new PriorityCache<String, EltValue>(0, 100, 10, 1000, costFunction);
+        assertTrue(pc.isEmpty());
+        pc.put("foo", new EltValue("foo", 1), 0);
+        pc.put("bar", new EltValue("bar", 1), 0);
+        pc.put("baz", new EltValue("baz", 1), 0);
+        assertFalse(pc.isEmpty());
+        assertEquals("3 elts expected", 3, pc.size());
+        assertEquals("foo", pc.get("foo").getId());
+        assertEquals("bar", pc.get("bar").getId());
+        assertEquals("baz", pc.get("baz").getId());
+
+        assertTrue(pc.contains("foo"));
+        assertTrue(pc.contains("bar"));
+        assertTrue(pc.contains("baz"));
+        assertFalse(pc.contains("quux"));
+
+        assertEquals("bar", pc.remove("bar").getId());
+        assertEquals(2, pc.size());
+        assertFalse(pc.contains("bar"));
+    }
+
+    @Test
+    public void testTotalCost() {
+        System.out.println("TotalCost");
+        PriorityCache<String, EltValue> pc = new PriorityCache<String, EltValue>(0, 100, 10, 1000, costFunction);
+        assertTrue(pc.isEmpty());
+        assertEquals(0, pc.getCurrentTotalCost());
+        pc.put("foo", new EltValue("foo", 10), 0);
+        assertEquals(10, pc.getCurrentTotalCost());
+        pc.put("bar", new EltValue("bar", 20), 0);
+        assertEquals(30, pc.getCurrentTotalCost());
+        pc.put("baz", new EltValue("baz", 100), 0);
+        assertEquals(130, pc.getCurrentTotalCost());
+
+        assertEquals("bar", pc.remove("bar").getId());
+        assertEquals(110, pc.getCurrentTotalCost());
+    }
+
+    @Test
+    public void testPriorities() {
+        System.out.println("Priorities");
+        PriorityCache<String, EltValue> pc = new PriorityCache<String, EltValue>(0, 100, 10, 500, costFunction);
+        assertTrue(pc.isEmpty());
+        assertEquals(0, pc.getCurrentTotalCost());
+        pc.put("c100-p50", new EltValue("1", 100), 50);
+        pc.put("c130-p40", new EltValue("2", 130), 40);
+        assertEquals(230, pc.getCurrentTotalCost());
+        pc.put("c80-p100", new EltValue("3", 80), 100);
+        assertEquals(310, pc.getCurrentTotalCost());
+        pc.put("c110-p60", new EltValue("4", 110), 60);
+        assertEquals(420, pc.getCurrentTotalCost());
+        assertEquals(4, pc.size());
+        pc.put("c150-p60", new EltValue("5", 150), 60); // cost 500 exceeded => lowest-prio element (c130-p40) evicted
+        assertEquals(4, pc.size());
+        assertEquals(440, pc.getCurrentTotalCost()); // 420 + 150 - 130
+        assertNull(pc.get("c130-p40"));
+
+        pc.put("c50-p40", new EltValue("6", 50), 40);
+        assertEquals(5, pc.size());
+        assertEquals(490, pc.getCurrentTotalCost());
+
+        pc.put("c200-p70", new EltValue("7", 200), 70); // should evict c50-p40, c100-p50, c110-p60 (NOT c150-p60 b/c of access-ordered eviction for equal-prio elts.)
+        assertEquals(3, pc.size());
+        assertEquals(430, pc.getCurrentTotalCost()); // 490 + 200 - 50 - 100 - 110
+        // check that the expected ones are still in there
+        assertEquals("3", pc.get("c80-p100").getId());
+        assertEquals("5", pc.get("c150-p60").getId());
+        assertEquals("7", pc.get("c200-p70").getId());
+
+        pc.setMaxTotalCost(200);   // should evict c150-p60, c200-p70
+        assertEquals(1, pc.size());
+        assertEquals(80, pc.getCurrentTotalCost());
+        assertEquals("3", pc.get("c80-p100").getId());
+    }
+
+}
